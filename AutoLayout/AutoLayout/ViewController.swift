046adbfc8a3ad52a87b7e0bbfca4148586c377aa
gurjot1829 <156744445+gurjot1829@users.noreply.github.com>
2025-07-21 21:26:24 +0530

    added AutoLayout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Gurjot Singh on 16/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
