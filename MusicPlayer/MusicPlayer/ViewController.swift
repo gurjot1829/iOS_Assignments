1047a7ee29de27747d7471ac48497cf809819519
gurjot1829 <156744445+gurjot1829@users.noreply.github.com>
2025-07-29 09:39:37 +0530

    Added MusicPlayer

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Gurjot Singh on 19/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MusicPlayer/MusicPlayerTests/MusicPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerTests.swift
+//  MusicPlayerTests
+//
+//  Created by Gurjot Singh on 19/07/25.
+//
+
+import Testing
+@testable import MusicPlayer
+
+struct MusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
